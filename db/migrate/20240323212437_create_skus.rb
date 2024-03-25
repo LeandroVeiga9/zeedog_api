d71d04fa329d16675b4eb7b41cf2a760e8ed4e2f
LeandroVeiga9 <lveiga301@gmail.com>
2024-03-25 19:35:10 -0300

    using rails 7.0

--- db/migrate/20240323212437_create_skus.rb
+++ db/migrate/20240323212437_create_skus.rb
@@ -1,4 +1,4 @@
-class CreateSkus < ActiveRecord::Migration[7.1]
+class CreateSkus < ActiveRecord::Migration[7.0]
   def change
     create_table :skus do |t|
       t.string :code, null: false, limit: 13
